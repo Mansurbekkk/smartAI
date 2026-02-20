e34f7fe122060fcec4d6ae724d7d43a351421a2e
Mansurbekkk <axmadaliyevmansur@gmail.com>
2026-02-20 17:21:55 +0500

    add app/src/main/java/com/example/myapplication/ui/theme/Color.kt

---
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SmartBlue = Color(0xFF4A90E2)
+val SmartGreen = Color(0xFF7ED321)
+val SmartYellow = Color(0xFFF5A623)
+val SmartDeepPurple = Color(0xFF4A148C)
+val SmartSlate = Color(0xFF263238)
+val SmartTeal = Color(0xFF00897B)
+
+// Kids Palette
+val KidsPrimary = SmartBlue
+val KidsSecondary = SmartYellow
+val KidsTertiary = SmartGreen
+
+// Teens Palette
+val TeensPrimary = SmartDeepPurple
+val TeensSecondary = SmartTeal
+val TeensTertiary = SmartSlate
